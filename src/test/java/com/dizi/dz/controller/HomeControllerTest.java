e1073f8da9c0b8cb0448b6c77a9e346411bdbc5e
amrrza <amirreza.esq@gmail.com>
2025-05-09 18:25:15 +0330

    Created Dizi-Cloud application following the Spring Boot in Action book, covering up to Chapter 1.

---
+++ src/test/java/com/dizi/dz/controller/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.dizi.dz.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class HomeControllerTest {
+
+    @Autowired
+    public MockMvc mockmvc;
+
+    @Test
+    public void testHomepage() throws Exception {
+        mockmvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to Dizisarayeh Duke's Comrades ...")));
+    }
+
+}
